18a1c85d72507e00a3088e8c5fb70ac7714bd648
tomboichik <tomboichik228@gmail.com>
2024-05-11 17:50:09 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/token_lsit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.token_lsit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:token_lsit/core/app.dart';
+
+void main() {
+  runApp(const App());
+}
